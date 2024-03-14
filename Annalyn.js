cb245023c0fec4523dac50a3260639a414c49837
hxniel <hnlromero@gmail.com>
2024-03-13 21:23:21 -0600

    comSimulación

--- Annalyn.js
+++ Annalyn.js
@@ -62,3 +62,45 @@ function arqueroDormido() {
   }
 
  
+   // Simulación
+ let seLiberaAmiga = true; // Variable para simular si se logra liberar a la amiga o no
+ while (seLiberaAmiga) {
+   let caballero = caballeroDormido();
+   let arquero = arqueroDormido();
+   let prisionero = prisioneroDespierto();
+   let perro = tienePerro();
+    console.log("\nEstado de los personajes: \n");
+   console.log("Caballero dormido:", caballero);
+   console.log("Arquero dormido:", arquero);
+   console.log("Prisionero despierto:", prisionero);
+   console.log("Annalyn tiene su perro:", perro);
+    // Llamamos a la función accionesDisponibles con los estados actuales de los personajes
+let acciones = accionesDisponibles(caballero, arquero, prisionero, perro);
+// Mostramos por consola las acciones disponibles en este turno
+console.log("\nAcciones disponibles:", acciones);
+
+
+// Si hay al menos una acción disponible
+if (acciones.length > 0) {
+ // Se elige una acción aleatoria entre las disponibles
+ let accion_actual = acciones[Math.floor(Math.random() * acciones.length)];
+ let accionAleatoria = accion_actual;
+ // Se ejecuta la acción seleccionada
+ realizarAccion(accionAleatoria);
+ acciones.slice(accion_actual);
+ seLiberaAmiga = Math.random() < 0.5;
+ if (seLiberaAmiga === true){
+     console.log("Con la acción de", accionAleatoria, "no se pudo liberar al prisionero");
+ }else{
+     console.log("Con la acción de", accionAleatoria, "se pudo liberar al prisionero");
+ }
+
+
+} else {
+ // Si no hay acciones disponibles, se muestra un mensaje y se termina el juego
+ console.log("No hay acciones disponibles. Fin del juego.");
+ seLiberaAmiga = false; // Terminamos el ciclo de liberación de la amiga
+}
+
+
+}
